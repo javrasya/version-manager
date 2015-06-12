427789ea9633d457392b4bf949787572b6fa0d17
Ahmet DAL <ceahmetdal@gmail.com>
2015-06-13 02:00:36 +0300

    version is upgraded to 0.2.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except IOError:
 
 setup(
     name='version-upgrader',
-    version='0.2.0',
+    version='0.2.1',
     description="Version upgrader in all spesific files like setup.py, package.json, bower.json etc.",
     author="Ahmet DAL",
     author_email="ceahmetdal@gmail.com",
