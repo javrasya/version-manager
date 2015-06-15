5792500d69ece1cef9103c0dc240fbed0336df22
Ahmet DAL <ceahmetdal@gmail.com>
2015-06-15 15:43:20 +0300

    binary files and setup file are excluded from coverage. File coverage is also increased to 100%

--- src/bin/current_version.py
+++ src/bin/current_version.py
@@ -3,7 +3,7 @@ from src.file import loaded_files
 
 def main():
     for file in loaded_files:
-        print "File %s version is %s" % (file.path, file.current_version)
+        print "File %s version is %s\n" % (file.path, file.current_version)
 
 
 if __name__ == '__main__':  # pragma: no cover
--- tests/test__file.py
+++ tests/test__file.py
@@ -1,6 +1,9 @@
 import os
 from unittest import TestCase
 from os.path import dirname
+from StringIO import StringIO
+import sys
+
 from src.file import File
 
 __author__ = 'ahmetdal'
@@ -10,6 +13,21 @@ class test_File(TestCase):
     def setUp(self):
         self.file = File("test_file", os.path.join(dirname(dirname(__file__)), 'test_resources', 'test_file'), '(?P<match_left>"version"\s*:\s*(?:"))(?P<version>(?:(?:\d+)+.?)+)(?P<match_right>")')
         self.file.update_version('0.2.0')
+
+    def test_regex_is_none(self):
+        try:
+            File("test_file", os.path.join(dirname(dirname(__file__)), 'test_resources', 'test_file'), None)
+            self.assertFalse(True)
+        except Exception, e:
+            self.assertEqual("File release regex can not be None.", e.message)
+
+    def test_no_version_definition_is_found(self):
+        out = StringIO()
+        sys.stdout = out
+        path = os.path.join(dirname(dirname(__file__)), 'test_resources', 'test_file')
+        self.file = File("test_file", path, '(?P<match_left>"version"\s*=\s*(?:"))(?P<version>(?:(?:\d+)+.?)+)(?P<match_right>")')
+        self.assertIsNone(self.file.current_version)
+        self.assertEqual("No version definition is found in file %s" % path, out.getvalue().strip())
 
     def test_current_version(self):
         # current_version test
@@ -21,3 +39,25 @@ class test_File(TestCase):
         self.file.update_version('0.2.1')
         self.assertEqual('0.2.1', self.file.current_version)
         self.file.update_version('0.2.0')
+
+    def test_update_with_invalid_version(self):
+        try:
+            self.file.update_version('a.b.c')
+            self.assertFalse(True)
+        except Exception, e:
+            self.assertTrue("Invalid version" in e.message)
+        self.assertEqual('0.2.0', self.file.current_version)
+
+    def test_next_version(self):
+        try:
+            self.file.next_version()
+            self.assertFalse(True)
+        except NotImplementedError:
+            pass
+
+    def test_previous_version(self):
+        try:
+            self.file.previous_version()
+            self.assertFalse(True)
+        except NotImplementedError:
+            pass
